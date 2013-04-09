38e1c8624d798486ba68c7a2df8ec6f26a8f8442
Marlus Araujo <mar251@gmail.com>
2013-04-09 17:22:20 -0300

    organizando o basico

--- public/js/app.js
+++ public/js/app.js
@@ -1,70 +1,63 @@
+// CONSOLE FIX
+
 if (typeof console == "undefined" || typeof console.log == "undefined") {
     var console = {
         log: function() {}
     };
 }
 
-function isNumber(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-}
-/*
-(function(window, $) {
+//// EMBER APP
 
-    var $window = $(window);
-
-    var APP = {
-
-        init: function(){       
-
-            $window.resize( this.resize );
-            $window.resize();
-
-            $('.menuit').on('click',function(){
-                window.current = $(this).attr('href');
-
-            })
-
-        },
-
-        resize: function(e) {
-
-
-        }
-    };
-
-window.APP = APP;
-
-}(window, jQuery));
-
-jQuery('document').ready(function($){
-    APP.init();
+window.App = Ember.Application.create({
+    // mostrar transicoes
+    LOG_TRANSITIONS: true,
+    // salvar path
+    currentPath: ''
 });
-*/
 
+//// ROUTES
 
-window.App = Ember.Application.create();
-
-
+// ROUTER PRINCIPAL
 
 App.Router.map(function() {
+    this.route("home");
     this.route("agenda");
     this.route("rede");
     this.route("blog");
-    this.resource('upac', { path: '/upac' }, function() {
-        this.route('quem');
+    this.resource('upac', function() {
         this.route('video');
         this.route('voce');
         this.route('contato');
     });
 });
 
-App.ApplicationRoute = Ember.Route.extend({
-  setupController: function(controller) {
-    // `controller` is the instance of ApplicationController
-    controller.set('title', "Hello world!");
-  }
+App.IndexRoute = Em.Route.extend({
+    redirect: function() {
+        this.transitionTo('home');   
+    }
 });
 
+//// CONTROLLERS
+
+// CONTROLLER PRINCIPAL
+
 App.ApplicationController = Ember.Controller.extend({
-  appName: 'UPAC'
+    route_class: null,
+    // variavel com hash atual
+    updateCurrentPath: function() {
+        var path_array = this.get('currentPath').split('.');
+        var route_class = path_array.length > 1 ? path_array[0] + ' ' + path_array.join('-') : path_array[0];
+        this.set('route_class',route_class);
+        App.set('currentPath', route_class);
+    }.observes('currentPath')
 });
+
+//// VIEWS
+
+// MENU PRINCIPAL
+
+App.MenuView = Em.View.extend({
+    templateName: 'menu'
+});
+
+
