711a72025a4fac1156123f72c44864f3328257c0
Marlus Araujo <mar251@gmail.com>
2013-04-10 20:56:24 -0300

    grunt handlebars compiler: limpando index com compilador no grunt

--- public/js/app.js
+++ public/js/app.js
@@ -58,8 +58,53 @@ App.ApplicationController = Ember.Controller.extend({
     }.observes('currentPath')
 });
 
-App.RedeView = Ember.View.extend({
-    templateName: 'mapa',
+//// VIEWS
+
+// MENU PRINCIPAL
+
+App.MenuView = Em.View.extend({
+    templateName: 'menu'
+});
+
+// SLIDESHOW HOME
+
+App.HomeSlidesView = Ember.View.extend({
+    templateName: 'home_slides',
+    t:-1,
+    timer: null,
+    tik: function(){
+        this.t = (this.t+1)%3;
+        this.$('.slideshow li').removeClass('show');
+        this.$('.slideshow li').eq(this.t).addClass('show').hide().fadeIn(500);
+        //console.log('tick',this.t);
+        this.timer = Ember.run.later(this, 'tik', 4000);
+    },
+    resize: function(e){
+        //this.$()
+    },
+    didInsertElement: function(){
+        //this.$().hide().show('slow');
+        this.$('.slideshow li').each(function(i,el){
+            var img = $(el).find('img');
+            var src = img.attr('src');
+            $(el).css({backgroundImage:'url('+src+')'});
+            //img.remove();
+        });
+        this.tik();
+        this.resize();
+        this.$(window).bind('resize',this.resize);
+    },
+    willDestroyElement: function(){
+        Ember.run.cancel(this.timer);
+        this.$(window).unbind('resize');
+    }
+    
+});
+
+// MAPA
+
+App.RedeMapaView = Ember.View.extend({
+    templateName: 'rede_mapa',
     handleClick: function(e){
        console.log( e.latLng );
     },
@@ -105,45 +150,5 @@ App.RedeView = Ember.View.extend({
     }
 });
 
-//// VIEWS
 
-// MENU PRINCIPAL
 
-App.MenuView = Em.View.extend({
-    templateName: 'menu'
-});
-
-// SLIDESHOW HOME
-
-App.HomeSlidesView = Ember.View.extend({
-    templateName: 'home_view',
-    t:-1,
-    timer: null,
-    tik: function(){
-        this.t = (this.t+1)%3;
-        this.$('.slideshow li').removeClass('show');
-        this.$('.slideshow li').eq(this.t).addClass('show').hide().fadeIn(500);
-        //console.log('tick',this.t);
-        this.timer = Ember.run.later(this, 'tik', 4000);
-    },
-    resize: function(e){
-        //this.$()
-    },
-    didInsertElement: function(){
-        //this.$().hide().show('slow');
-        this.$('.slideshow li').each(function(i,el){
-            var img = $(el).find('img');
-            var src = img.attr('src');
-            $(el).css({backgroundImage:'url('+src+')'});
-            //img.remove();
-        });
-        this.tik();
-        this.resize();
-        this.$(window).bind('resize',this.resize);
-    },
-    willDestroyElement: function(){
-        Ember.run.cancel(this.timer);
-        this.$(window).unbind('resize');
-    }
-    
-});
---
+++ public/js/templates.js
@@ -0,0 +1,177 @@
+Ember.TEMPLATES["agenda"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+  
+
+
+  data.buffer.push("<h1>Agenda</h1>");
+  
+});
+
+Ember.TEMPLATES["blog"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+  
+
+
+  data.buffer.push("<h1>BLOG</h1>");
+  
+});
+
+Ember.TEMPLATES["home"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+  var buffer = '', hashTypes, escapeExpression=this.escapeExpression;
+
+
+  data.buffer.push("<h1>\n	<span>Universidade</span>\n	<span>Popular</span>\n	<span>de Arte</span>\n	<span>e Ciência</span>\n</h1>\n\n");
+  hashTypes = {};
+  data.buffer.push(escapeExpression(helpers.view.call(depth0, "App.HomeSlidesView", {hash:{},contexts:[depth0],types:["ID"],hashTypes:hashTypes,data:data})));
+  return buffer;
+  
+});
+
+Ember.TEMPLATES["home_slides"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+  
+
+
+  data.buffer.push("<ul class=\"slideshow\">\n	<li><img src=\"./img/home01.jpg\"></li>\n	<li><img src=\"./img/home02.jpg\"></li>\n	<li><img src=\"./img/home03.jpg\"></li>\n</ul>");
+  
+});
+
+Ember.TEMPLATES["menu"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+  var buffer = '', stack1, stack2, hashTypes, options, escapeExpression=this.escapeExpression, self=this, helperMissing=helpers.helperMissing;
+
+function program1(depth0,data) {
+  
+  
+  data.buffer.push("<span><span class=\"ico\"></span>Entrada</span>");
+  }
+
+function program3(depth0,data) {
+  
+  
+  data.buffer.push("<span><span class=\"ico\"></span>Sobre a UPAC</span>");
+  }
+
+function program5(depth0,data) {
+  
+  
+  data.buffer.push("<span><span class=\"ico\"></span>Rede</span>");
+  }
+
+function program7(depth0,data) {
+  
+  
+  data.buffer.push("<span><span class=\"ico\"></span>Blog</span>");
+  }
+
+function program9(depth0,data) {
+  
+  
+  data.buffer.push("<span><span class=\"ico\"></span>Agenda</span>");
+  }
+
+function program11(depth0,data) {
+  
+  
+  data.buffer.push("<span><span class=\"ico\"></span>Fazer login</span>");
+  }
+
+  data.buffer.push("<nav id=\"main_menu\" ");
+  hashTypes = {'class': "STRING"};
+  data.buffer.push(escapeExpression(helpers.bindAttr.call(depth0, {hash:{
+    'class': ("route_class")
+  },contexts:[],types:[],hashTypes:hashTypes,data:data})));
+  data.buffer.push(">\n	<ul>\n		<li class=\"menu_it home\">");
+  hashTypes = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["STRING"],hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "home", options) : helperMissing.call(depth0, "linkTo", "home", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("</li>\n		<li class=\"menu_it upac\">");
+  hashTypes = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(3, program3, data),contexts:[depth0],types:["STRING"],hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "upac", options) : helperMissing.call(depth0, "linkTo", "upac", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("</li>\n		<li class=\"menu_it rede\">");
+  hashTypes = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(5, program5, data),contexts:[depth0],types:["STRING"],hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "rede", options) : helperMissing.call(depth0, "linkTo", "rede", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("</li>\n		<li class=\"menu_it blog\">");
+  hashTypes = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(7, program7, data),contexts:[depth0],types:["STRING"],hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "blog", options) : helperMissing.call(depth0, "linkTo", "blog", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("</li>\n		<li class=\"menu_it agenda\">");
+  hashTypes = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(9, program9, data),contexts:[depth0],types:["STRING"],hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "agenda", options) : helperMissing.call(depth0, "linkTo", "agenda", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("</li>\n	</ul>\n</nav>\n\n<div id=\"sec_menu\">\n	<li class=\"menu_it login\">");
+  hashTypes = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(11, program11, data),contexts:[depth0],types:["STRING"],hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "user.entrar", options) : helperMissing.call(depth0, "linkTo", "user.entrar", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("</li>\n</div>");
+  return buffer;
+  
+});
+
+Ember.TEMPLATES["rede"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+  var hashTypes, escapeExpression=this.escapeExpression;
+
+
+  hashTypes = {};
+  data.buffer.push(escapeExpression(helpers.view.call(depth0, "App.RedeMapaView", {hash:{},contexts:[depth0],types:["ID"],hashTypes:hashTypes,data:data})));
+  
+});
+
+Ember.TEMPLATES["rede_mapa"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+  
+
+
+  data.buffer.push("<div id=\"map_canvas\"></div>");
+  
+});
+
+Ember.TEMPLATES["upac"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+  
+
+
+  data.buffer.push("<h1 class=\"logo\">UPAC</h1>\n\n<p>Nossa proposta educacional está calcada em uma visão de educação que toma  a vivência como caminho e considera o saber-de- experiência-feito como ponto de partida, base da produção do conhecimento. Aqui referenciamos um dos nossos mestres, Paulo Freire, que nos ensina que o conhecimento provém da experiência. Nos ensina ainda, dialogando com nossa mestra maior, Nise da Silveira, que não se educa só pela racionalidade. Portanto apresentamos a amorosidade como princípio fundante de nosso caminho pedagógico e que gera o afeto catalisador, grande legado de Nise que, por sua vez, gera cuidado e alegria também conceitos que nos iluminam e orientam o nosso caminhar.</p>\n\n<iframe width=\"560\" height=\"315\" src=\"http://www.youtube.com/embed/XCVsm-_B_pU\" frameborder=\"0\" allowfullscreen></iframe>\n\n<p>Aprendemos com Baruch de Spinoza que somos afetados pelas paixões e que as paixões que geram alegria  despertam nossa potência de viver. Portanto as paixões alegres constituem-se também referencias de nossa pratica pedagógica. Também dialogando com Nise e Spinoza referendamos a idéia de Deus  como algo inerente ao humano, síntese entre transcendência e imanência, sagrado e profano, corpo e espírito. Apresentamos a vida como centralidade, biocentrismo em vez de antropocentrismo. Portanto nossa universidade referencia as praticas tradicionais como o xamanismo, o candomblé e tantas outras, como caminhos de aprendizagens significativas, de fortalecimento da identidade e dos desvelamento do que Jung chamou de  inconsciente coletivo.</p>\n\n<p>Na UPAC propomos uma ciência intuitiva, que considera a importância do ato criador e onde criação não se separa da invenção. Onde a poesia e a cultura popular revelam a beleza do conhecer  que gera luz, faz nascer novas possibilidades de transformação do cotidiano em suas complexidades. Referenciamos ainda Paulo Freire  ao compreendermos o humano como inacabado em sua incompletude que valoriza o saber do outro e da outra respeitando as diferenças e reconhecendo as semelhanças propondo o exercício da alteridade produtor de polifonias e policromias.</p>\n\n<p>Parte de uma visão ética que considera a fraternidade e a solidariedade como ideias-força e compreendendo a circularidade sistêmica, propõe mandalas como sínteses reflexivas e movimentos  em teias que referendam a conjugação do verbo esperançar como estratégia de conquista da liberdade.</p>\n\n<h2>UPAC na rede</h2>\n\n<p>Este site é uma plataforma de colaboração para os integrantes da UPAC e quem mais estiver interessado em ingressar na nossa rede.</p>\n\n<p>Navegue pelos conteúdos através do menu à esquerda. Cada botão leva a um espaço diferente:</p>\n\n<img src=\"./img/upac_sobre_icones.png\"/>\n\n<p>E para entrar com seu perfil ou se cadastrar, clique no botão à direita:</p>\n\n<img src=\"./img/upac_sobre_login.png\"/>\n\n<h2>Contato</h2>\n\n<p>\n	<strong>Universidade Popular de Arte e Ciência</strong>\n	<br/>Inst. Mun. de Assistência À Saúde Nise da Silveira\n	<br/>R. Ramiro Magalhães, 521 - Engenho de Dentro\n	<br/>Rio de Janeiro - RJ, 20730-460\n</p>\n\n<p><strong>contato@upac.br</strong></p>");
+  
+});
+
+Ember.TEMPLATES["user"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+  var buffer = '', hashTypes, escapeExpression=this.escapeExpression;
+
+
+  data.buffer.push("<h1>Acessar UPAC</h1>\n\n");
+  hashTypes = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "outlet", {hash:{},contexts:[depth0],types:["ID"],hashTypes:hashTypes,data:data})));
+  return buffer;
+  
+});
+
+Ember.TEMPLATES["user/entrar"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+  
+
+
+  data.buffer.push("teste!");
+  
+});
