b2a688c4bdb02ddc3d9f8e1371eed8d6b0e3a674
Tiago Rezende <tiagosr@gmail.com>
2013-04-13 13:55:28 -0300

    config sample

---
+++ config/config-sample.js
@@ -0,0 +1,15 @@
+module.exports = {
+	development: {
+		app: {
+			name: "UPAC"
+		},
+		db: "mongodb://localhost/upac",
+		secret: "your secret string here"
+	},
+	test: {
+
+	},
+	production: {
+
+	}
+}
