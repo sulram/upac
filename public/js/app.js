9201ce5bd94722a2263f701ed33fcd74988ccb22
Marlus Araujo <mar251@gmail.com>
2013-04-17 01:44:15 -0300

    começando mapa (e outros ajustes)

--- public/js/app.js
+++ public/js/app.js
@@ -19,6 +19,7 @@ window.App = Ember.Application.create({
 
 App.User = Ember.Object.extend({
   auth: {
+    id: null,
     loggedIn: false,
     username: null,
   },
@@ -63,12 +64,21 @@ App.Router.map(function() {
     this.resource("user",function(){
         this.route("cadastrar");
     });
+    this.resource("perfil");
     this.route('logout');
 });
 
 App.IndexRoute = Em.Route.extend({
     redirect: function() {
         this.transitionTo('home');   
+    }
+});
+
+
+
+App.RedeRoute = Em.Route.extend({
+    setupController: function(controller, song) {
+        App.MapController.getMarkers();
     }
 });
 
@@ -80,7 +90,8 @@ App.LogoutRoute = Ember.Route.extend({
             url: '/logout',
             success: function(data, status, jqXHR){
                 User.authenticate(data.auth);
-                _route.transitionTo('home');
+                //_route.transitionTo('home');
+                window.history.go(-1);
             }
         });
     }
@@ -92,6 +103,7 @@ App.LogoutRoute = Ember.Route.extend({
 
 App.ApplicationController = Ember.Controller.extend({
     route_class: null,
+    needs: ['rede'],
     // variavel com hash atual
     updateCurrentPath: function() {
         var path_array = this.get('currentPath').split('.');
@@ -100,6 +112,20 @@ App.ApplicationController = Ember.Controller.extend({
         App.set('currentPath', route_class);
     }.observes('currentPath')
 });
+
+App.RedeController = Ember.Controller.extend({
+    startMarking: function(){
+        App.MapController.startMarking();
+    },
+    finishMarking: function(){
+        App.MapController.finishMarking(true);
+    },
+    cancelMarking: function(){
+        App.MapController.finishMarking();
+    }
+});
+
+
 
 App.UserIndexController = Ember.Controller.extend({
     isPosting: false,
@@ -205,9 +231,6 @@ App.HomeSlidesView = Ember.View.extend({
 
 App.RedeMapaView = Ember.View.extend({
     templateName: 'rede_mapa',
-    handleClick: function(e){
-       console.log( e.latLng );
-    },
     handleZoom: function() {
         if (App.map.getZoom() < 3){
             App.map.setZoom(3);
@@ -243,13 +266,115 @@ App.RedeMapaView = Ember.View.extend({
         // aplica estilo
         App.map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
         // eventos
-        google.maps.event.addListener(App.map, 'click', this.handleClick);
+        google.maps.event.addListener(App.map, 'click', function(e){
+            App.MapController.createMark(e);
+        });
         google.maps.event.addListener(App.map, 'zoom_changed', this.handleZoom);
-
-        console.log('maps!');
     }
 });
 
+App.MapMarker = Ember.Object.extend({
+    latLng: null,
+    latitude: function () {return this.latLng.lat()}.property('latLng'),
+    longitude: function () {return this.latLng.lng()}.property('latLng'),
+    removeFromMap: function () {
+        i = App.markers.get('content').indexOf(this);
+        App.MapMarkers.removeAt(i);
+    },
+    markerClick: function () {
+        App.MapMarkers.set('selection', this);
+    }
+});
+
+App.MapController = Em.Object.create({
+    isMarking: false,
+    isFetching: true,
+    markers: [],
+    getMarkers: function(){
+        
+        var that = this;
+        that.set('isFetching',true);
+
+        $.ajax({
+            type: 'GET',
+            url: '/users',
+            data: { limit: 0 },
+            success: function(data, status, jqXHR){
+                User.authenticate(data.auth);
+                $.each(data.users, function(i,el){
+                    //var user = data.users[u];
+                    if(el.geo.length){
+                        console.log('user',el.geo);
+                        that.markers.push(new google.maps.Marker({
+                            position: new google.maps.LatLng(el.geo[0],el.geo[1]),
+                            map: App.map
+                        }));
+                    }
+                });
+            that.set('isFetching',false);
+            },
+            error: function(jqXHR,status,error){
+                console.log(jqXHR);
+                _self.authenticate(JSON.parse(jqXHR.responseText));
+                that.set('isFetching',false);
+            }
+        });
+    },
+    startMarking: function(){
+        this.set('isMarking',true);
+        if(App.MapController.userPosition){
+            App.MapController.userMark = new google.maps.Marker({
+                position: e.latLng,
+                map: App.map
+            });
+        }
+    },
+    finishMarking: function(save){
+        var that = this;
+        if(save){
+            var pos = [
+                App.MapController.userPosition.jb,
+                App.MapController.userPosition.kb
+            ];
+            $.ajax({
+                type: 'PUT',
+                url: '/user/'+User.get('auth.id'),
+                data: { geo: pos },
+                success: function(data, status, jqXHR){
+                    User.authenticate(data.auth);
+                    that.set('isMarking',false);
+                    console.log('saved',pos);
+                    that.getMarkers();
+                },
+                error: function(jqXHR,status,error){
+                    console.log(jqXHR);
+                    _self.authenticate(JSON.parse(jqXHR.responseText));
+                    
+                }
+            });
+        }
+        
+    },
+    createMark: function(e){
+
+        if(!App.MapController.isMarking){
+            
+            console.log( 'get position!', e.latLng );
+
+        } else {
+
+            console.log( 'mark!', e.latLng.kb );
+            
+            if(App.MapController.userMark) App.MapController.userMark.setMap(null);
+
+            App.MapController.userPosition = e.latLng;
+            App.MapController.userMark = new google.maps.Marker({
+                position: e.latLng,
+                map: App.map
+            });
+        }
+    }
+});
 
 //// MODELS
 
