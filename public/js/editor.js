b31feeafbc7bf2a794190915429016926963df54
Tiago Rezende <tiagosr@gmail.com>
2013-06-05 18:11:40 -0300

    Showdown do milhão
    Live preview de markdown -> html

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,7 +1,17 @@
+var delay, showdown = new Showdown.converter();
 var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
 	mode: 'markdown',
 	lineNumbers: true,
     lineWrapping: true,
 	theme: "default",
 	extraKeys: {"Enter": "newlineAndIndentContinueMarkdownList"}
+});
+var updatePreview = function() {
+	console.log(editor.getValue());
+	console.log(showdown.makeHtml(editor.getValue()));
+	$("#preview").html(showdown.makeHtml(editor.getValue()));
+}
+editor.on("change", function() {
+	clearTimeout(delay);
+	delay = setTimeout(updatePreview, 300);
 });
