29aaaa6ae0a22dc8f8f730b3fe53bc96f7fb0a1b
Marlus Araujo <mar251@gmail.com>
2013-06-29 14:56:16 -0300

    organizando codigo do ML

--- public/js/libs/redactor/v9/medialibrary.js
+++ public/js/libs/redactor/v9/medialibrary.js
@@ -1,8 +1,9 @@
 if (typeof RedactorPlugins === 'undefined') var RedactorPlugins = {};
 
 RedactorPlugins.medialibrary = {
-    init: function()
-    {   
+
+    init: function(){   
+
         var _this = this;
 
         var article_id = $('body').data('article-id');
@@ -13,6 +14,8 @@ RedactorPlugins.medialibrary = {
         var $btn_add = $('#medialibrary #img_add');
         var $btn_cover = $('#medialibrary #img_cover');
         var $imgs = $('#medialibrary #images-gallery .gallery-image');
+
+        // clique na miniaturas carregadas
 
         $(document).on('click', '#images-gallery figure.loaded', function() {
             $('#images-gallery figure').removeClass('selected');
@@ -61,39 +64,56 @@ RedactorPlugins.medialibrary = {
             maxFilesize: 5,
             acceptedMimeTypes:'image/gif,image/jpeg,image/pjpeg,image/png,image/x-windows-bmp,image/bmp',
             init: function() {
+
                 this.on("success", function(file, xhr, elem) {
 
                     var image = xhr.image;
                     console.log(image);
 
+                    // adiciona a lista images[] (input hidden)
+
                     var $input = $('<input name="images[]" type="hidden">').attr('value', image._id);
                     $("#images-hidden").append($input);
 
+                    // cria figure e imagem na galeria
+
                     var $img = $('<figure><img/><span class="icon fui-heart"></span></figure>');
                     $img.data('image-id', image._id);
+                    $("#images-gallery").append($img);
 
-                    // verificar se a imagem já tem miniaturas criadas
+                    // carregar miniaturas
 
                     var try_loading_image;
 
                     try_loading_image = function() {
+
                         console.log('try_loading_image', image._id);
+
                         $.getJSON('/image/'+image._id,{},function(data, status, xhr) {
+
                             if(data && data.image && data.image.upload_complete) {
+                                
                                 console.log('loaded_image', image._id, data.image.sizes);
+                                
                                 $img.addClass('loaded')
                                 $img.attr('data-id', image._id);
                                 $img.attr('data-src', _.find(data.image.sizes, function(size) { return size.size == 'large'; }).cdn_url);
                                 $img.find('img').attr('src', _.find(data.image.sizes, function(size) { return size.size == 'icon'; }).cdn_url);
+
+                                // seleciona ultima imagem
+
                                 $('#images-gallery figure.loaded:last').click();
+
                             } else {
+                                
+                                // tenta novamente
+
                                 setTimeout(try_loading_image, 3000);
                             }
                         });
                     }
-                    setTimeout(try_loading_image, 3000);
 
-                    $("#images-gallery").append($img);
+                    setTimeout(try_loading_image, 3000);
 
                     // remove da lista de uploads
 
@@ -106,6 +126,8 @@ RedactorPlugins.medialibrary = {
                 });
             }
         });
+        
+        // callback do plugin
 
         var callback = $.proxy(function(){
 
@@ -116,7 +138,9 @@ RedactorPlugins.medialibrary = {
 
         this.buttonAddAfter('link', 'medialibrary', 'Imagens', callback);
     },
+
     openModal: function(){
+
         $('#medialibrary').addClass('show');
         // TODO: checar se existe imagem, senao, upload
         if($("#images-gallery figure").length){
@@ -125,13 +149,20 @@ RedactorPlugins.medialibrary = {
         } else {
             this.selectTab('upload');
         }
+
     },
+
     closeModal: function(){
+
         this.selectTab(null);
         $('#medialibrary').removeClass('show');
+
     },
+
     selectTab: function(tab){
+
         this.tabs.removeClass('active').has('a[href=#'+tab+']').addClass('active');
         this.panels.removeClass('show').filter('.'+tab).addClass('show');
+
     }
 }
